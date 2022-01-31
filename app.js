513f58c52df3a341358b65a47261c0b1447e34ad
Olin Kirkland <me@olinkirk.land>
2022-02-01 00:17:37 +0100

    Add search column and display castle ids - adjust styling so the list doesn't go off the page

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-// Fetch data
+// Load JSON data
 const url = 'json-data/all.json';
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => loadData(data));
+
+let entries;
+
+function loadData(data) {
+  entries = data;
+
+  refreshList();
+}
+
+function refreshList() {
+  for (const entry in entries) {
+    let item = document.createElement('li');
+    item.appendChild(document.createTextNode(entry));
+
+    var list = document.getElementById('search-results');
+    list.appendChild(item);
+  }
+}
